40d827716ea773bafe9fae36cb617098b2a3d302
Michael Giebner <mikegiebner@gmail.com>
2019-09-26 09:59:31 -0400

    Finished Section 19, Chapter 216, .on() event listener

--- Section19,chapter213.js
+++ Section19,chapter213.js
@@ -1,24 +1,59 @@
-$("h1").click(myConsoleLog)
+// $("h1").click(myConsoleLog)
+
+// function myConsoleLog() {
+//    console.log("You clicked the h1")
+// }
+
+// // Now add a click listener to all buttons
+// $("button").click(function() {
+//    console.log("You clicked a button")
+// })
+
+// // All buttons have the same click listener, now id the button that was clicked
+// // Can't use this since .click() is jQuery, we have to use the jQuery version
+// // of this which is $(this)
+// $("button").click(function() {
+//    console.log("You clicked the button " + $(this).text())
+// })
+
+// // Chapter 214, .keypress()
+// // e contains all the information about the jQuery .keypress() event
+// $("input").keypress(function(e) {
+//    // console.log("You pressed the " + $(this).val() + " char")
+//    // console.log("You pressed the " + e.which + " char")
+
+//    // If the user hits the enter key
+//    if (e.which === 13) {
+//       console.log("You hit the enter key after typing " + $(this).val())
+//    }
+// })
+
+//==============================================================================
+//==============================================================================
+//    Re-do the code above using .on()
+//==============================================================================
+//==============================================================================
+$("h1").on("click", myConsoleLog)
 
 function myConsoleLog() {
    console.log("You clicked the h1")
 }
 
 // Now add a click listener to all buttons
-$("button").click(function() {
+$("button").on("click", function() {
    console.log("You clicked a button")
 })
 
 // All buttons have the same click listener, now id the button that was clicked
 // Can't use this since .click() is jQuery, we have to use the jQuery version
 // of this which is $(this)
-$("button").click(function() {
+$("button").on("click", function() {
    console.log("You clicked the button " + $(this).text())
 })
 
 // Chapter 214, .keypress()
 // e contains all the information about the jQuery .keypress() event
-$("input").keypress(function(e) {
+$("input").on("keypress", function(e) {
    // console.log("You pressed the " + $(this).val() + " char")
    // console.log("You pressed the " + e.which + " char")
 
@@ -27,3 +62,6 @@ $("input").keypress(function(e) {
       console.log("You hit the enter key after typing " + $(this).val())
    }
 })
+
+// Add a class to each button
+$("button").addClass("button")
